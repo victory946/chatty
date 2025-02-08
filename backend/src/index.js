21ba8e3071a632330b384d9eba72f303bf148950
ukaegbu victorygold <you@example.com>
2025-02-08 10:47:43 +0100

    chat

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express";
 
 import path from "path";
 
-import { connectDB } from "./lib/db.js";
+import { connectDB } from "./src/lib/db.js";
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { app, server } from "./src/lib/socket.js";
 
 dotenv.config();
 
